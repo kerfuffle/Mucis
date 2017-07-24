f0fa70f15f86a8d19632598becea0114f92ac885
kerfuffle <thepolarofbears@gmail.com>
2017-07-23 21:14:23 -0400

    Setup Server

---
+++ Code/PC/.metadata/.plugins/org.eclipse.core.resources/.history/f2/8075fa440d7000171dbdd690f3b956df
@@ -0,0 +1,8 @@
+package net.kerfuffle.MucisServer;
+
+public class Global {
+
+	public static final String SP = ",";
+	
+	public static final int LOGIN = 0;	
+}
--- Code/PC/MucisServer/src/net/kerfuffle/MucisServer/Global.java
+++ Code/PC/MucisServer/src/net/kerfuffle/MucisServer/Global.java
@@ -4,5 +4,8 @@ public class Global {
 
 	public static final String SP = ",";
 	
-	public static final int LOGIN = 0;	
+	public static final int LOGIN = 0,
+							ADD_SONG = 1,
+							REMOVE_SONG = 2,
+							LIBRARY = 3;	
 }
